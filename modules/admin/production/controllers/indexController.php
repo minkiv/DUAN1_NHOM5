bb93b7eb383ae8214b2544bee5834629ed4515af
minkiv <92282454+minkiv@users.noreply.github.com>
2022-11-18 11:28:24 +0700

    logo, add product
    change logo, add product function

--- modules/admin/production/controllers/indexController.php
+++ modules/admin/production/controllers/indexController.php
@@ -18,14 +18,27 @@ function createAction() {
 function createPostAction() {
     $name = $_POST['name'];
     $description = $_POST['description'];
+    $category_id = $_POST['category_id'];
+    $price = $_POST['price'];
+    $count = $_POST['count'];
+    $status=$_POST['status'];
+    $thumb = $_FILES['thumb']['name'];
+    $target_dir = "../upload/";
+    $target_file = $target_dir . basename($_FILES["thumb"]["name"]);
+        if (move_uploaded_file($_FILES["thumb"]["tmp_name"], $target_file)) {
+                    //echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                } else {
+                    //echo "Sorry, there was an error uploading your file.";
+                }
+
     if (empty($name)) {
-        push_notification('danger', ['Vui lòng nhập vào tên danh mục']);
-        header('Location: ?role=admin&mod=category&action=create');
+        push_notification('danger', ['Vui lòng nhập vào tên sản phẩm']);
+        header('Location: ?role=admin&mod=production&action=create');
         die();
     }
-    create_category($name, $description);
+    create_production($name, $description,$category_id,$price,$count,$status,$thumb);
     push_notification('success', ['Tạo mới danh mục sản phẩm thành công']);
-    header('Location: ?role=admin&mod=category');
+    header('Location: ?role=admin&mod=production');
 }
 
 function deleteAction() {
