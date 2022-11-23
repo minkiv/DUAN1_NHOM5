bf44d144d0346bbfbce49a0cb88635d69d883b40
Min <tunnalee@gmail.com>
2022-11-23 19:57:00 +0700

    chx

--- modules/admin/home/views/indexView.php
+++ modules/admin/home/views/indexView.php
@@ -7,11 +7,34 @@
             <div class="d-flex align-items-center flex-wrap mr-2">
                 <!--begin::Page Title-->
                 <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">Tổng quan</h5>
+                
                 <!--end::Page Title-->
             </div>
+           
             <!--end::Info-->
         </div>
     </div>
+    
+    <div class="header">
+            <div class="logo">
+                
+            </div>
+            <div class="menu">
+                <img src="/images/logo.png" alt="">
+                <ul>
+                    <li>
+                        <a href="">Trang chủ</a>
+                        <a href="">Giới thiệu</a>
+                        <a href="">Sản phẩm</a>
+                        <a href="">Tin tức</a>
+                        <a href="">Liên hệ</a>
+                        <input type="text" placeholder="Tìm kiếm" >
+                    </li>
+                </ul>
+            </div>
+            
+            
+    
     <!--end::Subheader-->
     <!--begin::Entry-->
     <div class="d-flex flex-column-fluid">
