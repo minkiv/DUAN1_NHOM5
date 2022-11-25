5ad895dc124780970bf23bf2675ec7316f4f2e3c
Min <tunnalee@gmail.com>
2022-11-25 14:11:05 +0700

    loadproduct

--- modules/client/product/views/indexView.php
+++ modules/client/product/views/indexView.php
@@ -1,8 +1,4 @@
-<?php
- echo "<pre>";
-  print_r($products);
- echo "</pre>";
-?>
+
 
 
 <!DOCTYPE html>
@@ -12,14 +8,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href=".public/css/stylepro.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="./public/css/stylepro.css">
+    
 </head>
 <body>
     <div class="container">
         <div class="header">
             <div class="logo">
-                <img src=".public/images/logo.png" alt="Ảnh logo">
+                <img src="./public/images/mainlogo.png" alt="Ảnh logo">
             </div>
 
             <div class="search">
