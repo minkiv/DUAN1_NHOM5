ef85ef6e5c4d680eeba0c68d6e93aa1f5ece7335
minkiv <92282454+minkiv@users.noreply.github.com>
2022-11-25 18:44:07 +0700

    product page

---
+++ modules/client/product/controllers/indexController.php
@@ -0,0 +1,11 @@
+<?php
+
+function construct() {
+    load_model('index');
+}
+
+function indexAction() {
+    $data['products']=get_list_productions();
+    $data['categories'] = get_list_categories();
+    load_view('index',$data);
+}
