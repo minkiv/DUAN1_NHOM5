e67dabeca35a344441475b7263e18c6a07e9f0f8
phungati <taipvph23695@fpt.edu.vn>
2022-11-25 20:21:29 +0700

    model

--- modules/client/product/models/indexModel.php
+++ modules/client/product/models/indexModel.php
@@ -3,3 +3,17 @@ function get_list_users() {
     $result = db_fetch_array("SELECT * FROM `tbl_users`");
     return $result;
 }
+function get_list_productions() {
+    $result = db_fetch_array("SELECT * FROM `productions`");
+    return $result;
+}
+
+function get_list_categories() {
+    $result = db_fetch_array("SELECT c.id, c.name, c.description, c.create_id, c.created_at, u.full_name, u.id as `uid` FROM `categories` c JOIN `users` u ON c.create_id = u.id");
+    return $result;
+}
+
+function get_one_production($id) {
+    $result = db_fetch_row("SELECT * FROM `productions` WHERE `productions`.`id` = $id");
+    return $result;
+}
