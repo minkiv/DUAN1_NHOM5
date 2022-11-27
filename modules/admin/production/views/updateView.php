a8d011d84aad786969c58b61c3de129c41e1cd32
phuonggg123 <118293022+phuonggg123@users.noreply.github.com>
2022-11-27 23:30:19 +0700

    pk

--- modules/admin/production/views/updateView.php
+++ modules/admin/production/views/updateView.php
@@ -1,5 +1,5 @@
 <?php get_header('', 'Chỉnh sửa sản phẩm') ?>
-    
+
     <!--begin::Subheader-->
     <div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
         <div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
